2fa9c93a86cef2388d1bbaa6b9506b9b1c042062
shakivhusain <shakibraza436@gmail.com>
2024-09-21 15:10:08 +0400

    Stack Implementation

--- app/Index.tsx
+++ app/Index.tsx
@@ -1,38 +1,18 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
 
-export default function Page() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.subtitle}>Inde Screen</Text>
-      <Link href='/HomeScreen'> Home Screen</Link>
-      <Link href="/AboutScreen"> About Screen</Link>
-      <Link href="/ContactScreen"> Contact Screen</Link>
+const index = () => {
+    return (
+        <View>
 
+            <Text > Title Of Home Screen</Text>
+            <Link href='/HomeScreen'> Home Screen</Link>
+            <Link href="/AboutScreen"> About Screen</Link>
+            <Link href="/ContactScreen"> Contact Screen</Link>
 
-
-    </View>
-  );
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+export default index
